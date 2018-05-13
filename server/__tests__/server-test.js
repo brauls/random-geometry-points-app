830a357e66ea094c8f48fb9d90a36a2c51394f50
Benedikt Rauls <brauls101@gmail.com>
2018-05-13 14:48:57 +0200

    update gitignore

---
+++ server/__tests__/server-test.js
@@ -0,0 +1,5 @@
+describe("server", () => {
+  test("test1", () => {
+    expect(2).toEqual(2);
+  });
+});
--- server/server.test.js
+++
@@ -1,5 +0,0 @@
-describe('server', () => {
-  test('test1', () => {
-    expect(2).toEqual(2);
-  });
-});
