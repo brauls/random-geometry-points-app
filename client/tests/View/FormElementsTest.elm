0b745e2aaa0df830826b5aa79d495ae901ba7667
Benedikt Rauls <brauls101@gmail.com>
2018-05-26 18:57:43 +0200

    add tests for form elements

---
+++ client/tests/View/FormElementsTest.elm
@@ -0,0 +1,54 @@
+module View.FormElementsTest exposing (..)
+
+import Fuzz exposing (string)
+import Html.Attributes as Attr
+import Test exposing (Test, describe, fuzz)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, id, tag, text)
+import View.FormElements exposing (GeometryFormParam, geometryForm)
+
+
+testFormElements : Test
+testFormElements =
+    describe "Test the FormElements module"
+        [ fuzz string "Test the form row label" <|
+            \geometryName ->
+                geometryForm geometryName [ geometryFormParam ] ""
+                    |> Query.fromHtml
+                    |> Query.find
+                        [ tag "label"
+                        , id ("label-" ++ geometryName ++ "-radius")
+                        , attribute <| Attr.for ("input-" ++ geometryName ++ "-radius")
+                        ]
+                    |> Query.has [ text "radius" ]
+        , fuzz string "Test the form row input" <|
+            \geometryName ->
+                geometryForm geometryName [ geometryFormParam ] ""
+                    |> Query.fromHtml
+                    |> Query.find
+                        [ tag "input"
+                        , id ("input-" ++ geometryName ++ "-radius")
+                        , "label-"
+                            ++ geometryName
+                            ++ "-radius"
+                            |> Attr.attribute "aria-labelledby"
+                            |> attribute
+                        ]
+                    |> Query.has []
+        , fuzz string "Test the form row description label" <|
+            \geometryName ->
+                geometryForm geometryName [ geometryFormParam ] ""
+                    |> Query.fromHtml
+                    |> Query.find
+                        [ tag "small"
+                        , id ("label-description-" ++ geometryName ++ "-radius")
+                        ]
+                    |> Query.has [ text "the radius description" ]
+        ]
+
+
+geometryFormParam : GeometryFormParam
+geometryFormParam =
+    { name = "radius"
+    , description = "the radius description"
+    }
