08df9b0a1b547e0e6bf077d9352b418d7d640ead
Benedikt Rauls <brauls101@gmail.com>
2018-05-31 22:22:01 +0200

    add test cases for models

--- client/tests/ModelsTest.elm
+++ client/tests/ModelsTest.elm
@@ -10,6 +10,18 @@ testModels =
     describe "Test the Models module"
         [ test "initialModel" <|
             \_ -> Expect.equal expectedModel (initialModel HomeRoute)
+        , test "initialPlaneParams - no error as default" <|
+            \_ ->
+                initialPlaneParams
+                    |> List.map (\param -> param.error == NoError)
+                    |> List.foldr (&&) True
+                    |> Expect.true "Expected the initial paramter to contain no error"
+        , test "initialPlaneParams - empty string as default value" <|
+            \_ ->
+                initialPlaneParams
+                    |> List.map (\param -> param.value == "")
+                    |> List.foldr (&&) True
+                    |> Expect.true "Expected the initial paramter to have an empty string as value"
         ]
 
 
