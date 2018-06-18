c1d10b4160b472d16193c4f8f970021ab98284f2
Benedikt Rauls <brauls101@gmail.com>
2018-06-18 23:33:07 +0200

    add test cases for server.js

--- server/__tests__/server-test.js
+++ server/__tests__/server-test.js
@@ -1,5 +1,96 @@
+import request from "supertest";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+const mockAxios = new MockAdapter(axios);
+
+import app from "../server.js";
+
 describe("server", () => {
   test("test1", () => {
     expect(2).toEqual(2);
+  });
+  test("test random plane points - failure with message", () => {
+    mockAxios
+      .onGet(
+        "http://random-geometry-points-service.herokuapp.com/random-plane-points/3d/",
+        {}
+      )
+      .reply(400, {
+        message: "Invalid plane definition supplied"
+      });
+    const testUrl = "/random-plane-points";
+    return request(app)
+      .get(testUrl)
+      .then(
+        response => {
+          mockAxios.reset();
+          expect(response.statusCode).toEqual(400);
+          expect(response.text).toEqual("Invalid plane definition supplied");
+          return Promise.resolve();
+        },
+        _ => Promise.reject("Error during test")
+      );
+  });
+  test("test random plane points - failure without message", () => {
+    mockAxios
+      .onGet(
+        "http://random-geometry-points-service.herokuapp.com/random-plane-points/3d/",
+        {}
+      )
+      .reply(400, {});
+    const testUrl = "/random-plane-points";
+    return request(app)
+      .get(testUrl)
+      .then(
+        response => {
+          mockAxios.reset();
+          expect(response.statusCode).toEqual(400);
+          expect(response.text).toEqual("Unexpected error");
+          return Promise.resolve();
+        },
+        _ => Promise.reject("Error during test")
+      );
+  });
+  test("test random plane points - success", () => {
+    const baseUrl =
+      "http://random-geometry-points-service.herokuapp.com/random-plane-points/3d/";
+    const mockUrl = `${baseUrl}?radius=3&num_points=10&ref_x=-1.5&ref_y=3&ref_z=2.34&n_x=1&n_y=0&n_z=0`;
+    mockAxios.onGet(mockUrl, {}).reply(200, [
+      {
+        x_coord: 1,
+        y_coord: 2,
+        z_coord: 3
+      },
+      {
+        x_coord: -1.5,
+        y_coord: 2.23,
+        z_coord: 0
+      }
+    ]);
+    const testUrl =
+      "/random-plane-points?radius=3&number=10&x=-1.5&y=3&z=2.34&i=1&j=0&k=0";
+    return request(app)
+      .get(testUrl)
+      .then(
+        response => {
+          mockAxios.reset();
+          expect(response.statusCode).toEqual(200);
+          expect(response.body).toEqual([
+            {
+              x: 1,
+              y: 2,
+              z: 3
+            },
+            {
+              x: -1.5,
+              y: 2.23,
+              z: 0
+            }
+          ]);
+          return Promise.resolve();
+        },
+        _ => Promise.reject("Error during test")
+      );
   });
 });
--- server/server.js
+++ server/server.js
@@ -4,10 +4,8 @@ import axios from "axios";
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  console.log("production");
   app.use("/", express.static(__dirname + "/../client/build"));
 } else {
-  console.log("develop");
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin");
@@ -16,49 +14,35 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get("/hello", (req, res) => {
-  res.send("Hello world!");
-});
-
 app.get("/random-plane-points", (req, res) => {
   const url =
     "http://random-geometry-points-service.herokuapp.com/random-plane-points/3d/";
-  console.log("incoming request");
-  console.log(req.query);
-
   const params = parseQueryParams(req.query);
-
   const urlWithParams = createUrl(url, params);
-
-  console.log("starting request with url", urlWithParams);
-
   axios
     .get(urlWithParams)
     .then(response => {
-      console.log("response", response);
       const randomPoints = response.data.map(pnt => ({
         x: pnt.x_coord,
         y: pnt.y_coord,
         z: pnt.z_coord
       }));
       res.send(randomPoints);
-      console.log(randomPoints);
     })
     .catch(error => {
-      console.log("an error occured");
       if (error.response) {
-        console.error("server error", error.response.status);
-        console.error("body", error.response.data);
-        return res.status(500).send("error");
+        if (error.response.data && error.response.data.message) {
+          return res
+            .status(error.response.status)
+            .send(error.response.data.message);
+        }
+        return res.status(error.response.status).send("Unexpected error");
       } else if (error.request) {
-        console.error("no server response");
-        return res.status(500).send("error");
+        return res.status(503).send("Network error");
       } else {
-        console.error("error when setting up request");
-        return res.status(500).send("error");
+        return res.status(500).send("Unexpected error");
       }
     });
-  // res.send("getting plane points");
 });
 
 app.listen(5000, () => {
@@ -97,3 +81,5 @@ const getParamApiName = paramType => {
       return "num_points";
   }
 };
+
+export default app;
