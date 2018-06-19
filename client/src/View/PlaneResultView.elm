cda39ada6b5ed9156321b6e750bf20d8235fe3f8
Benedikt Rauls <brauls101@gmail.com>
2018-06-19 22:38:27 +0200

    show plane parameters in caption text

--- client/src/View/PlaneResultView.elm
+++ client/src/View/PlaneResultView.elm
@@ -2,19 +2,51 @@ module View.PlaneResultView exposing (view)
 
 import Html exposing (Html, caption, div, table, tbody, td, text, th, thead, tr)
 import Html.Attributes exposing (class, id, scope)
-import Models exposing (GeometryParam, GeometryParamType, GeometryType, Model)
+import Models exposing (GeometryParam, Model, getParamTypeName)
+import Selectors exposing (hasPlaneFormError)
 import View.Navigator exposing (navbar)
 import View.RandomPoints3DView as RandomPoints3DView
 
 
 view : Model -> Html msg
 view model =
+    let
+        planeParams =
+            model.planeParameters
+
+        caption =
+            captionText planeParams
+    in
     div [ id "plane-result-view" ]
         [ navbar
-        , RandomPoints3DView.view captionText model.randomPlanePoints
+        , RandomPoints3DView.view caption model.randomPlanePoints
         ]
 
 
-captionText : String
-captionText =
-    "The randomly created plane points"
+captionText : List GeometryParam -> String
+captionText planeParams =
+    let
+        isPlaneValid =
+            not (hasPlaneFormError planeParams)
+
+        parseParam =
+            \param ->
+                ", "
+                    |> (++) param.value
+                    |> (++) "="
+                    |> (++) (getParamTypeName param.paramType)
+
+        paramsFormatted =
+            planeParams
+                |> List.filter (\param -> not (param.paramType == Models.PointCount))
+                |> List.map parseParam
+                |> List.reverse
+                |> List.foldl (++) ""
+                |> String.dropRight 2
+    in
+    case isPlaneValid of
+        True ->
+            """The randomly created plane points. The plane definition was: """ ++ paramsFormatted
+
+        False ->
+            ""
--- client/tests/View/PlaneResultViewTest.elm
+++ client/tests/View/PlaneResultViewTest.elm
@@ -32,12 +32,132 @@ testPlaneView =
                     |> Query.find [ tag "table" ]
                     |> Query.children []
                     |> Query.count (Expect.equal 3)
+        , test "view - check caption for plane without input" <|
+            \_ ->
+                planeResultHtml
+                    |> Query.fromHtml
+                    |> Query.find [ tag "caption" ]
+                    |> Query.hasNot [ text "The randomly created plane points." ]
+        , test "view - check caption for plane without invalid input" <|
+            \_ ->
+                planeResultHtmlWithInvalidPlaneParams
+                    |> Query.fromHtml
+                    |> Query.find [ tag "caption" ]
+                    |> Query.hasNot [ text "The randomly created plane points." ]
+        , test "view - check caption for plane with valid input" <|
+            \_ ->
+                planeResultHtmlWithValidPlaneParams
+                    |> Query.fromHtml
+                    |> Query.find [ tag "caption" ]
+                    |> Query.has
+                        [ """The randomly created plane points. The plane definition was: x=1, y=2, z=3, i=1, j=0, k=0, radius=4.5""" |> text
+                        ]
         ]
 
 
 planeResultHtml : Html Msg
 planeResultHtml =
     div []
-        [ initialModel HomeRoute
+        [ initialModel PlaneResultRoute
+            |> view
+        ]
+
+
+planeResultHtmlWithInvalidPlaneParams : Html Msg
+planeResultHtmlWithInvalidPlaneParams =
+    let
+        initModel =
+            initialModel PlaneResultRoute
+
+        invalidPlaneParams =
+            [ { paramType = PointCount
+              , value = ""
+              , error = NoError
+              }
+            , { paramType = X
+              , value = ""
+              , error = NotInt
+              }
+            , { paramType = Y
+              , value = ""
+              , error = NotFloat
+              }
+            , { paramType = Z
+              , value = ""
+              , error = NotPositive
+              }
+            , { paramType = I
+              , value = ""
+              , error = NoError
+              }
+            , { paramType = J
+              , value = ""
+              , error = NoError
+              }
+            , { paramType = K
+              , value = ""
+              , error = NoError
+              }
+            , { paramType = Radius
+              , value = ""
+              , error = NoError
+              }
+            ]
+
+        invalidPlaneModel =
+            { initModel | planeParameters = invalidPlaneParams }
+    in
+    div []
+        [ invalidPlaneModel
+            |> view
+        ]
+
+
+planeResultHtmlWithValidPlaneParams : Html Msg
+planeResultHtmlWithValidPlaneParams =
+    let
+        initModel =
+            initialModel PlaneResultRoute
+
+        validPlaneParams =
+            [ { paramType = PointCount
+              , value = "10"
+              , error = NoError
+              }
+            , { paramType = X
+              , value = "1"
+              , error = NoError
+              }
+            , { paramType = Y
+              , value = "2"
+              , error = NoError
+              }
+            , { paramType = Z
+              , value = "3"
+              , error = NoError
+              }
+            , { paramType = I
+              , value = "1"
+              , error = NoError
+              }
+            , { paramType = J
+              , value = "0"
+              , error = NoError
+              }
+            , { paramType = K
+              , value = "0"
+              , error = NoError
+              }
+            , { paramType = Radius
+              , value = "4.5"
+              , error = NoError
+              }
+            ]
+
+        validPlaneModel =
+            { initModel | planeParameters = validPlaneParams }
+    in
+    div []
+        [ validPlaneModel
             |> view
         ]
